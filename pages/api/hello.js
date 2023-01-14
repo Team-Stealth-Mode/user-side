2772807364576935bc854c86d1c42388a53ce89b
Tilak Dave <davetilak003@gmail.com>
2023-01-14 14:50:30 +0530

    update

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import db from "../../lib/firebase-admin"
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(_, res) {
+  const scs = await db.collection("users").doc("UpgpGpzXKfR7IvSY6QeQk1DNUyd2").get();
+  res.status(200).json({ scs: scs.data().social_credit_score });
 }
